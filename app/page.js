33354fdd1887af89c6f783a342eab2251d800065
mohammadkabir2003 <rabbi.fr123@gmail.com>
2024-07-31 18:47:38 -0400

    new project

--- app/page.js
+++ app/page.js
@@ -1,95 +1,138 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import {
+  Box,
+  Stack,
+  Typography,
+  TextField,
+  InputAdornment,
+} from "@mui/material";
+import KitchenIcon from "@mui/icons-material/Kitchen";
+import { Unstable_NumberInput as NumberInput } from "@mui/base/Unstable_NumberInput";
+
+const list = [
+  "eggs",
+  "milk",
+  "bread",
+  "rice",
+  "carrot",
+  "mango",
+  "banana",
+  "nuts",
+  "berries",
+  "fruits",
+  "apples",
+  "tomatos",
+];
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+    <Box
+      width="100vw"
+      height="100vh"
+      display="flex"
+      flexDirection={"column"}
+      bgcolor="white"
+    >
+      <Box
+        width="auto"
+        height="100px"
+        display={"flex"}
+        justifyContent={"center"}
+        alignContent="center"
+        color="black"
+        py={4}
+      >
+        <Typography variant="h2" align="center">
+          EZPantry
+        </Typography>
+      </Box>
+      <Box
+        width="auto"
+        height="100px"
+        display={"flex"}
+        justifyContent={"center"}
+        alignItems="center"
+        color="black"
+        py={4}
+      >
+        <Typography variant="h4" align="center">
+          Keep track of everything in your pantry.
+        </Typography>
+      </Box>
+      <Box
+        width="100%"
+        height="400px"
+        bgcolor="lightgrey"
+        display="flex"
+        justifyContent={"space-between"}
+        alignItems="center"
+        p={4}
+      >
+        <Box
+          width="50%"
+          height="300px"
+          p={2}
+          color="black"
+          display="flex"
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
+          <Box display="flex" flexDirection="column">
+            <Typography variant="h5" align="center" gutterBottom color="black">
+              Have new items to add? Type the new item you want to add below.
+            </Typography>
+            <TextField
+              id="filled-basic"
+              label="Add Items"
+              variant="filled"
+              size="medium"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <KitchenIcon />
+                  </InputAdornment>
+                ),
+              }}
             />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+          </Box>
+        </Box>
+        <Box
+          width="50%"
+          height="400px"
+          display="flex"
+          flexDirection="column"
+          py={4}
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          <Typography variant="h5" align="center" gutterBottom color="black">
+            List of Items in the Pantry
+          </Typography>
+          <Stack
+            width="100%"
+            height="auto"
+            spacing={2}
+            overflow={"auto"}
+            px={8}
+          >
+            {list.map((i) => (
+              <Box
+                key={i}
+                height="100px"
+                width="auto"
+                display={"flex"}
+                flexDirection={"row"}
+                justifyContent={"center"}
+                alignItems={"center"}
+                gap={4}
+                p={2}
+                bgcolor="lightgray"
+                color="black"
+                sx={{ border: "2px solid grey" }}
+              >
+                <Typography>{i}</Typography>
+              </Box>
+            ))}
+          </Stack>
+        </Box>
+      </Box>
+    </Box>
   );
 }
